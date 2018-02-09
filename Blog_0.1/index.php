ff3336696efea6ac7ce481f6d219b13979a4bfe3
404NatF <2501181146@qq.com>
2018-02-09 11:54:41 +0800

    第一次提交

---
+++ Blog_0.1/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>My Blog</title>
+<link rel="stylesheet" type="text/css" href="styles/1/basic.css">
+<link rel="stylesheet" type="text/css" href="styles/1/index.css">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+</head>
+<body>
+	<div id="header">
+		<h1><a href="index.php">博客多用户管理系统</a></h1>
+		<ul>
+			<li>首页</li>
+			<li>注册</li>
+			<li>登录</li>
+			<li>个人中心</li>
+			<li>风格</li>
+			<li>管理</li>
+			<li>退出</li>
+		</ul>
+	</div>
+	<div id="list">
+		<h2>帖子列表</h2>
+	</div>
+	<div id="user">
+		<h2>新进会员</h2>
+	</div>
+	<div id="pics">
+		<h2>最新图片</h2>
+	</div>
+	<div id="footer">
+		<p>404NatF 版权所有 侵权必究</p>
+	</div>
+</body>
+</html>
